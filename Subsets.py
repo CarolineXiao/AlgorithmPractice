6490967330c33a690768436d94f285fcd4376dd2
CarolineXiao <carolinexiaojiayue@gmail.com>
2020-03-09 22:13:02 -0400

    Create Subsets.py

---
+++ Subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+    """
+    def subsets(self, nums):
+        # write your code here
+        subset = [None] * len(nums)
+        result = []
+
+        self.helper(nums, subset, result, 0)
+        return result
+        
+    def helper(self, nums, subset, result, i):
+        if i == len(nums):
+            result.append(list(filter(lambda x: x is not None, subset)))
+        else:    
+            subset[i] = None
+            self.helper(nums, subset, result, i+1)
+            subset[i] = nums[i]
+            self.helper(nums, subset, result, i+1)
