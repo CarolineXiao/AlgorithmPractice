25e8706e044bc8af9f7084a1e987b4a52dd396fb
CarolineXiao <carolinexiaojiayue@gmail.com>
2020-03-12 13:03:14 -0400

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,17 +1,18 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        num_dict = {}
+class Solution:
+    """
+    @param numbers: An array of Integer
+    @param target: target = numbers[index1] + numbers[index2]
+    @return: [index1, index2] (index1 < index2)
+    """
+    def twoSum(self, numbers, target):
+        # write your code here
+        # using hashset
+        dict = {}
+        for i in range(len(numbers)):
+            n = target - numbers[i]
+            if n in dict:
+                return [dict[n], i]
+            dict[numbers[i]] = i
         
-        for i in range(len(nums)):
-            sub = target - nums[i]
-            if sub not in num_dict:
-                num_dict[nums[i]] = i
-            else:
-                return {num_dict[sub], i}
                 
 # Complexity: O(n)
