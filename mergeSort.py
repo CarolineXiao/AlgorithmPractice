89a0218d67c56ab7b38149bd47b344caa1547e2e
CarolineXiao <carolinexiaojiayue@gmail.com>
2020-03-15 13:08:08 -0400

    Create mergeSort.py

---
+++ mergeSort.py
@@ -0,0 +1,46 @@
+class Solution:
+    """
+    @param A: an integer array
+    @return: nothing
+    """
+    def sortIntegers2(self, A):
+        # MergeSort
+        temp = [0] * len(A)
+        self.mergeSort(A, 0, len(A) - 1, temp)
+        
+    def mergeSort(self, A, start, end, temp):
+        if start >= end:
+            return
+        
+        self.mergeSort(A, start, (start+end)//2, temp)
+        self.mergeSort(A, (start+end)//2 + 1, end, temp)
+        self.merge(A, start, end, temp)
+    
+    def merge(self, A, start, end, temp):
+        middle = (start + end) // 2
+        leftIndex = start
+        rightIndex = middle + 1
+        tempIndex = leftIndex
+        
+        while leftIndex <= middle and rightIndex <= end:
+            if A[leftIndex] <= A[rightIndex]:
+                temp[tempIndex] = A[leftIndex]
+                tempIndex += 1
+                leftIndex += 1
+            else:
+                temp[tempIndex] = A[rightIndex]
+                tempIndex += 1
+                rightIndex += 1
+            
+        while leftIndex <= middle:
+            temp[tempIndex] = A[leftIndex]
+            tempIndex += 1
+            leftIndex += 1
+        while rightIndex <= end:
+            temp[tempIndex] = A[rightIndex]
+            tempIndex += 1
+            rightIndex += 1
+        
+        for i in range(start, end+1):
+            A[i] = temp[i]
+       
