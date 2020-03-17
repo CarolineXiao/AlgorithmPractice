2b049922908da48fbe772c24d3463b6900b52ad5
CarolineXiao <carolinexiaojiayue@gmail.com>
2020-03-17 13:44:02 -0400

    Update Subsets.py
    two methods

--- Subsets.py
+++ Subsets.py
@@ -4,23 +4,40 @@ class Solution:
     @return: A list of lists
     """
     def subsets(self, nums):
-        # write your code here
+        '''
+        # simple recursion
         if nums == None:
             return []
+        nums.sort()
+        result = []
+        subset = []
+        self.dfs(nums, result, subset, 0)
+        return result
+    
+    def dfs(self, nums, result, subset, index):
+        if index == len(nums):
+            result.append(list(subset))
+            return
         
+        subset.append(nums[index])
+        self.dfs(nums, result, subset, index+1)
+        subset.remove(nums[index])
+        self.dfs(nums, result, subset, index+1)
+        '''
+        
+        # DFS - backtracking
+        if nums == None:
+            return []
         nums.sort()
         result = []
         subset = []
         self.dfs(nums, subset, result, 0)
         return result
     
-    def dfs(self, nums, subset, result, index):
-        if index == len(nums):
-            result.append(list(subset))
-            return
+    def dfs(self, nums, subset, result, startIndex):
+        result.append(list(subset))
+        for i in range(startIndex, len(nums)):
+            subset.append(nums[i])
+            self.dfs(nums, subset, result, i+1)
+            subset.remove(nums[i])
             
-        
-        subset.append(nums[index])
-        self.dfs(nums, subset, result, index+1)
-        subset.remove(nums[index])
-        self.dfs(nums, subset, result, index+1)
