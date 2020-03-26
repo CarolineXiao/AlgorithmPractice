d125694ab4f93d64e676010fcaaaa8af82a6d555
CarolineXiao <carolinexiaojiayue@gmail.com>
2020-03-26 14:30:37 -0400

    4 ways

--- Triangle.py
+++ Triangle.py
@@ -1,3 +1,4 @@
+import sys
 class Solution:
     """
     @param triangle: a list of lists of integers
@@ -37,6 +38,7 @@ class Solution:
         '''
         
         # 3. Memoization
+        '''
         if triangle == []:
             return 0
         
@@ -66,4 +68,30 @@ class Solution:
         memo[x][y] = min(left,right) + triangle[x][y]
         
         return memo[x][y]
+        '''
+    
+    # 4. DP
+        if triangle == []:
+            return 0
         
+        f = [[sys.maxsize] * len(triangle[i]) for i in range(len(triangle))]
+        #f[0][0] = triangle[0][0]
+        
+        for i in range(len(triangle)):
+            for j in range(len(triangle[i])):
+                if i - 1 < 0:
+                    f[i][j] = triangle[i][j]
+                else:
+                    if 0 <= j < len(triangle[i-1]) and 0 <= j - 1 < len(triangle[i-1]):
+                        f[i][j] = min(f[i-1][j-1], f[i-1][j]) + triangle[i][j]
+                    elif 0 <= j < len(triangle[i-1]):
+                        f[i][j] = f[i-1][j] + triangle[i][j]
+                    else:
+                        f[i][j] = f[i-1][j-1] + triangle[i][j]
+        
+        min_sum = sys.maxsize
+        for s in f[len(triangle)-1]:
+            if s < min_sum:
+                min_sum = s
+        return min_sum
+                    
