2ed59cb077172b70fc0dbcec1a2b758d2a09e14c
CarolineXiao <carolinexiaojiayue@gmail.com>
2020-04-09 16:06:19 -0400

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -4,35 +4,28 @@ class Solution:
     @return: Find all unique triplets in the array which gives the sum of zero.
     """
     def threeSum(self, numbers):
-        # write your code here
-        if numbers == None or numbers == []:
+        if numbers == []:
             return []
-        
-        result = []
+            
         numbers.sort()
-        n = len(numbers)
-        for i in range(n):
-            if i > 0 and numbers[i] == numbers[i-1]:
-                continue
-            self.twoSum(numbers, -numbers[i], i, result)
-        
-        return result
-        
-    def twoSum(self, numbers, target, i, result):
-        left = i + 1
-        right = len(numbers) - 1
+        results = []
+        for i in range(len(numbers)):
+            target = numbers[i]
+            self.twoSum(numbers, target, i+1, len(numbers)-1, results)
+            
+        return results
+            
+            
+    def twoSum(self, numbers, target, left, right, results):
         while left < right:
-            if numbers[left] + numbers[right] == -numbers[i]:
-                result.append([numbers[i], numbers[left], numbers[right]])
-                while left < right and numbers[left] == numbers[left+1]:
-                    left += 1
-                while left < right and numbers[right] == numbers[right-1]:
-                    right -= 1
+            if numbers[left] + numbers[right] + target == 0:
+                result = [target, numbers[left], numbers[right]]
+                
+                if result not in results:
+                    results.append(result)
                 left += 1
                 right -= 1
-            elif numbers[left] + numbers[right] < -numbers[i]:
+            elif numbers[left] + numbers[right] + target < 0:
                 left += 1
             else:
                 right -= 1
-                
-        
